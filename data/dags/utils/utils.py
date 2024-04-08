dcfaa8afade01ee916d7aae8835f0c95de777e3a
Rthorski <alexandre.rethore@icloud.com>
2024-04-08 23:10:57 +0200

    refacto utils.py

--- data/dags/utils/utils.py
+++ data/dags/utils/utils.py
@@ -1,11 +1,11 @@
 from google.cloud import storage
 from dotenv import dotenv_values
 
+config = dotenv_values('/opt/airflow/dags/.env')
+
 def authentificateServiceAccount():
   
-  config = dotenv_values('/opt/airflow/dags/.env')
   credentials = config["GOOGLE_APPLICATION_CREDENTIALS"]
-  print(credentials)
   
   try:
     storage_client = storage.Client.from_service_account_json(credentials)
