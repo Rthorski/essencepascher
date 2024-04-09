c10bd127b0dbb7f034fa36c64c1606f9546915d3
Rthorski <alexandre.rethore@icloud.com>
2024-04-09 13:29:52 +0200

    add some variables

--- data/dags/utils/utils.py
+++ data/dags/utils/utils.py
@@ -1,7 +1,9 @@
 from google.cloud import storage
 from dotenv import dotenv_values
+from sqlalchemy import create_engine
 
 config = dotenv_values('/opt/airflow/dags/.env')
+engine = create_engine(f'postgresql+psycopg2://{config["POSTGRES_USER"]}:{config["POSTGRES_PASSWORD"]}@{config["POSTGRES_HOST"]}:5432/{config["POSTGRES_DB"]}')
 
 def authentificateServiceAccount():
   
@@ -13,3 +15,7 @@ def authentificateServiceAccount():
     print(f"Erreur: {e}")
     
   return storage_client
+
+storage_client = authentificateServiceAccount()
+bucket_name = 'essencepascher_files'
+bucket = storage_client.bucket(bucket_name)
