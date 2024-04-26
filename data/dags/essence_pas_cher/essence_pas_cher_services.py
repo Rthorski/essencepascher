04531ad11c9f4bc636575dcc7932db37aa41130a
Rthorski <alexandre.rethore@icloud.com>
2024-04-26 14:19:48 +0200

    refacto load tables

--- data/dags/essence_pas_cher/essence_pas_cher_services.py
+++ data/dags/essence_pas_cher/essence_pas_cher_services.py
@@ -7,13 +7,17 @@ from io import BytesIO
 import zipfile
 import psycopg
 import pandas as pd
+import pytz
+
+timezone_paris = pytz.timezone('Europe/Paris')
+heure_fr = datetime.now(timezone_paris)
 
 def rename_object_name(url):
   
   object_name = url.split('/')
   object_name = object_name[-2:]
   object_name = (', ').join(object_name)
-  datetime_now = datetime.now()
+  datetime_now = heure_fr
   object_name = str(datetime_now) + f"_{object_name}.xml"
   object_name = object_name.replace(' ', '').replace(',', '_')
   return object_name
@@ -63,6 +67,7 @@ def create_dataframes(root):
   services = []
   prix = []
   ruptures = []
+  fermetures = []
 
   for pdv in root.findall('pdv'):
     pdv_dict = pdv.attrib
@@ -90,34 +95,149 @@ def create_dataframes(root):
         prix.append({**child.attrib, **station_id_dict})
       elif child.tag == 'rupture':
         ruptures.append({**child.attrib, **station_id_dict})
-        
+      elif child.tag == 'fermeture':
+        fermetures.append({**child.tag, **station_id_dict})        
     stations.append(pdv_dict)
     
-  return [{"stations": stations}, {"horaires": jours}, {"services": services}, {"prix": prix}, {"ruptures": ruptures}]
-  
-  
-def load_tables_in_database(list_of_dict):
+  return [{"stations": stations}, {"horaires": jours}, {"services": services}, {"prix": prix}, {"ruptures": ruptures}, {"fermetures": fermetures}]
+
+
+def load_tables_in_database(list_of_dict, date_ti):
   
   for couple_of_attributs in list_of_dict:
     
     for name, dict_df in couple_of_attributs.items():
       
-      df = pd.DataFrame(dict_df)
+      if name == 'stations':
+        load_stations_table(dict_df, name, date_ti)
       
-      df.to_sql(
-          name=f"{name}",
-          con=engine,
-          if_exists='replace',
-          schema="dev",
-          method=psql_insert_copy,
-          index=False
-        )
+      elif name == 'services' or name == 'horaires':
+        load_services_and_horaires_table(dict_df, name)
+      
+      elif name == 'prix':
+        get_new_rows_and_load(dict_df, name, date_ti)
+        
+      elif name == 'ruptures':
+        get_new_rows_and_load(dict_df, name, date_ti)
+        
+        pass
 
 
-def drop_dev_schema_database():
+def get_new_rows_and_load(dict_df, name, date_ti):
+  
+  df = if_table_exists(dict_df, name, date_ti, primary_key=False)
+  
+  if df is not None:
+    df_donnees_entrantes = pd.DataFrame(dict_df) 
+    search_new_lines = pd.merge(df_donnees_entrantes, df, how="left", indicator=True)
+    news_rows = search_new_lines.loc[search_new_lines["_merge"] == "left_only"]
+    news_rows.drop(columns="_merge", inplace=True)
+    news_rows["injected_at"].fillna(heure_fr, inplace=True)
+    insert_into_table(news_rows, name)
+  
+
+def insert_into_table(df, name):
+  
+  values_sql = ','.join(['%s'] * len(df.columns))
+  columns = list(df.columns)
+  columns_tuple = tuple(columns)
+  col_without_apostrophes = ', '.join(columns_tuple)
+  query = f"""
+    INSERT INTO dev.{name} ({col_without_apostrophes})
+    VALUES ({values_sql})
+  """
   
   with psycopg.connect(conn_psycopg) as conn:
     with conn.cursor() as cur:
-      cur.execute('DROP SCHEMA if exists dev CASCADE;')
-      cur.execute('CREATE SCHEMA dev;')
+      cur.executemany(query, [tuple(row) for row in df.values])
+  
+
+def load_services_and_horaires_table(dict_df, name):
+  
+  df = pd.DataFrame(dict_df)
+  try:
+    with psycopg.connect(conn_psycopg) as conn:
+      with conn.cursor() as cur:
+        cur.execute(f"DROP TABLE dev.{name} CASCADE;")
+  except:
+    print(f"la table {name} n'existe pas")
+  
+  df.to_sql(
+  name=name,
+  con=engine,
+  if_exists="replace",
+  schema="dev",
+  method=psql_insert_copy,
+  index=False
+  )
+  
+        
+def load_stations_table(dict_df, name, date_ti):
+  
+  df_query = if_table_exists(dict_df, name, date_ti, primary_key=True)
+  
+  if df_query is not None:
+    date_max_of_scheduling = str(df_query["date_of_scheduling"].max())
+
+    if date_ti > date_max_of_scheduling:
+      df_stations = pd.DataFrame(dict_df)
+      df_stations['id'] = df_stations['id'].astype('int')
+      search_new_lines = pd.merge(df_stations, df_query, how="left", indicator=True)
+      news_rows = search_new_lines.loc[search_new_lines["_merge"] == "left_only"]
       
+      if news_rows.shape[0] > 0:
+        news_rows.drop(columns="_merge", inplace=True)
+        news_rows["injected_at"] = heure_fr
+        news_rows["date_of_scheduling"] = date_ti
+        news_rows["date_of_scheduling"] = pd.to_datetime(df_query["date_of_scheduling"], yearfirst=True)
+        insert_and_update_rows(news_rows)
+
+
+def insert_and_update_rows(df):
+
+  values_sql = ','.join(['%s'] * len(df.columns))
+  
+  with psycopg.connect(conn_psycopg) as conn:
+    with conn.cursor() as cur:
+      
+      update_query = "ON CONFLICT (id) DO UPDATE SET " + \
+                    ", ".join([f"{col} = EXCLUDED.{col}" for col in df.columns if col != 'id'])
+      
+      insert_query = f"INSERT INTO dev.stations (id, latitude, longitude, cp, pop, adresse, ville, injected_at, date_of_scheduling) VALUES ({values_sql}) {update_query}"
+      cur.executemany(insert_query, [tuple(row) for row in df.values])
+  
+
+def if_table_exists(dict_df, name, date_ti, primary_key):
+  
+  try:
+    df_query = pd.read_sql_query(
+    sql=f"SELECT * FROM dev.{name}",
+    con=engine
+  )
+    return df_query
+  
+  except:
+    df = pd.DataFrame(dict_df)
+    df["injected_at"] = heure_fr
+    if name == 'stations':
+      df["date_of_scheduling"] = date_ti
+      df["date_of_scheduling"] = pd.to_datetime(df["date_of_scheduling"], yearfirst=True)
+      df['id'] = df["id"].astype('int')
+    print("first load")
+          
+    df.to_sql(
+      name=name,
+      con=engine,
+      if_exists="replace",
+      schema="dev",
+      method=psql_insert_copy,
+      index=False
+    )
+    if primary_key:
+      query = """
+        ALTER TABLE dev.{}
+        ADD PRIMARY KEY (id);
+      """.format(name)
+      with psycopg.connect(conn_psycopg) as conn:
+        with conn.cursor() as cur:
+          cur.execute(query)
