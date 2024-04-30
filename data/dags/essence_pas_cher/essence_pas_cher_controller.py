233b824726de25bbced8401f38148534769748ca
Rthorski <alexandre.rethore@icloud.com>
2024-04-30 15:43:10 +0200

    add process_id variable

--- data/dags/essence_pas_cher/essence_pas_cher_controller.py
+++ data/dags/essence_pas_cher/essence_pas_cher_controller.py
@@ -1,5 +1,6 @@
 from essence_pas_cher.essence_pas_cher_services import get_data_from_api, upload_on_gcp_bucket
-from essence_pas_cher.essence_pas_cher_services import parseFile, create_dataframes, drop_dev_schema_database, load_tables_in_database
+from essence_pas_cher.essence_pas_cher_services import parseFile, create_dataframes
+from essence_pas_cher.essence_pas_cher_load import load_tables_in_database
 
 def upload_on_gcp(**context):
   
@@ -12,9 +13,10 @@ def upload_on_gcp(**context):
 def load_to_database(**context):
   ti = context['ti']
   date_ti = context['ts']
+  process_id = context['params']['process_id']
   object_name = ti.xcom_pull(key='object_name', task_ids='upload_on_gcp')
   root = parseFile(object_name)
   list_of_dict = create_dataframes(root)
-  load_tables_in_database(list_of_dict, date_ti)
+  load_tables_in_database(list_of_dict, date_ti, process_id)
     
   
