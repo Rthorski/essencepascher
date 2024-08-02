b25fbc46ee1f86ece7df676485ea55d6955fbd9a
Rthorski <alexandre.rethore@icloud.com>
2024-08-02 08:23:41 +0200

    création clés étrangères station_id

--- data/dags/essence_pas_cher/essence_pas_cher_load.py
+++ data/dags/essence_pas_cher/essence_pas_cher_load.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from essence_pas_cher.essence_pas_cher_services import heure_fr, heure_fr_str
+from essence_pas_cher.essence_pas_cher_services import heure_fr_str
 import psycopg
-from utils.connexions import conn_psycopg, psql_insert_copy, engine
+import psycopg2
+from utils.connexions import conn_psycopg, psql_insert_copy, engine, conn_psycopg2
 from datetime import datetime
-import numpy as np
 import pytz
 
 
@@ -17,8 +17,8 @@ def load_tables_in_database(list_of_dict, date_ti, process_id):
       if name == 'prix':
         load_prix_table(dict_df, name, process_id, date_ti)
         
-      elif name == 'stations':
-        load_services_and_horaires_and_stations_table(dict_df, name)
+      if name == 'stations':
+        load_stations_table(dict_df, name, date_ti)
 
       elif name == 'services':
         load_services_and_horaires_and_stations_table(dict_df, name)
@@ -28,6 +28,8 @@ def load_tables_in_database(list_of_dict, date_ti, process_id):
         
       elif name == 'ruptures':
         load_ruptures_table(dict_df, name)
+
+
 
 
 def delete_rows_with_process_id(name, process_id):
@@ -59,6 +61,24 @@ def delete_rows_with_process_id(name, process_id):
   print("----------------fin suppression ligne process_id-------")
 
 
+def get_all_stations():
+  get_all_stations = """
+    SELECT
+      id as id_station
+    FROM
+      dev.stations
+  """
+
+  try:
+      df = pd.read_sql_query(
+      sql=get_all_stations,
+      con=engine
+      )
+  except Exception as e:
+    raise e
+  
+  return df
+
 def load_prix_table(dict_df, name, process_id, date_ti):
   
   delete_rows_with_process_id(name, process_id)
@@ -80,26 +100,34 @@ def load_prix_table(dict_df, name, process_id, date_ti):
   parse_dates = 'maj'
   
   df_table_prix = stg_get_table_with_where_condition(columns=columns, name=name, dtype=dtype, where_condition=where_condition, parse_dates=parse_dates)
-  
+
   news_rows = search_new_or_updated_rows(df_table=df_table_prix, df_entry_data=df_data_entry)
 
+  df = get_all_stations()
+  
   if news_rows.shape[0] > 0:
     news_rows["process_id"] = process_id
     news_rows = news_rows[['nom', 'id', 'maj', 'valeur', 'station_id', 'injected_at', 'year', 'process_id', 'year_month']]
 
-    print(f"nb de nouveaux prix: {news_rows.shape}")
+    df["id_station"] = pd.to_numeric(df["id_station"], downcast='integer')
+    news_rows = news_rows.merge(df, how='inner', left_on='station_id', right_on='id_station')
+    news_rows = news_rows[['nom', 'id', 'maj', 'valeur', 'station_id', 'injected_at', 'year', 'process_id', 'year_month']]
 
+    print("nouveaux prix", news_rows.shape)
     print("début injection nouvelles lignes")
-    news_rows.to_sql(
-      name="prix",
-      con=engine,
-      schema='dev',
-      if_exists='append',
-      index=False,
-      method=psql_insert_copy
-    )
-    print("fin injection nouvelles lignes")
-    
+    try:
+      news_rows.to_sql(
+        name="prix",
+        con=engine,
+        schema='dev',
+        if_exists='append',
+        index=False,
+        method=psql_insert_copy
+      )
+      print("fin injection nouvelles lignes")
+    except Exception as e:
+        print(f"Erreur lors de l'insertion des données: {e}")
+        raise e
   else:
     print("pas de tarifs à jour")
 
@@ -107,21 +135,33 @@ def load_prix_table(dict_df, name, process_id, date_ti):
 def load_services_and_horaires_and_stations_table(dict_df, name):
   
   df = pd.DataFrame(dict_df)
+    
+  if name != 'stations':
+    df["station_id"] = pd.to_numeric(df["station_id"], downcast='integer')
+    df_stations = get_all_stations()
+    df = df.merge(df_stations, how='inner', left_on='station_id', right_on='id_station')
+    df.drop(columns=['id_station'], inplace=True)
+
   try:
-    with psycopg.connect(conn_psycopg) as conn:
+    with conn_psycopg2 as conn:
       with conn.cursor() as cur:
-        cur.execute(f"DROP TABLE dev.{name} CASCADE;")
-  except:
-    print(f"la table {name} n'existe pas")
-  
-  df.to_sql(
-  name=name,
-  con=engine,
-  if_exists="replace",
-  schema="dev",
-  method=psql_insert_copy,
-  index=False
-  )
+        cur.execute("DELETE FROM dev.{};".format(name))
+  except psycopg2.Error as e:
+    print(f"Erreur lors de la suppression de la table {name}: {e}")
+
+    
+  try:
+    df.to_sql(
+    name=name,
+    con=engine,
+    if_exists="append",
+    schema="dev",
+    index=False,
+    method=psql_insert_copy
+    )
+  except Exception as e:
+    print(f"Erreur lors de l'insertion des données: {e}")
+    raise e
   
   
 def load_stations_table(dict_df, name, date_ti):
@@ -129,7 +169,7 @@ def load_stations_table(dict_df, name, date_ti):
   date_ti_timzeone_europe_paris = get_date_ti_in_timezone_europe_paris(date_ti)
   str_date_ti_timzeone_europe_paris = date_ti_timzeone_europe_paris.strftime("%Y-%m-%dT%H:%M:%S%z")
 
-  columns = ('id, latitude, longitude, cp, pop, adresse, ville, date_of_scheduling')
+  columns = ('id, latitude, longitude, cp, pop, adresse, ville')
   
   dtype = {
     'id': 'int',
@@ -138,28 +178,23 @@ def load_stations_table(dict_df, name, date_ti):
     'cp': 'object',
     'pop': 'object',
     'adresse': 'object',
-    'ville': 'object',
-    'date_of_scheduling': 'string'
+    'ville': 'object'
   }
   
   df_table_stations = get_table_to_dataframe(columns=columns, name=name, dtype=dtype)
-  date_max_of_scheduling = str(df_table_stations["date_of_scheduling"].max())
-  df_table_stations.drop(columns=['date_of_scheduling'], inplace=True)
+  # date_max_of_scheduling = str(df_table_stations["date_of_scheduling"].max())
+  # df_table_stations.drop(columns=['date_of_scheduling'], inplace=True)
   
-  if str_date_ti_timzeone_europe_paris > date_max_of_scheduling:
 
-    df_entry_data = read_data_entry(dict_df=dict_df)
-    
-    df_entry_data = get_entry_data(df=df_entry_data, dtype=dtype)
+  df_entry_data = read_data_entry(dict_df=dict_df)
+  
+  df_entry_data = get_entry_data(df=df_entry_data, dtype=dtype)
 
-    news_rows = search_new_or_updated_rows(df_table=df_table_stations,
-                                          df_entry_data=df_entry_data)
-    
-    news_rows["date_of_scheduling"] = str_date_ti_timzeone_europe_paris
-    print(f"taille de la table stations: {df_table_stations.shape}")
-    print(f"taille des données entrantes: {df_entry_data.shape}")
-    print(f"nouvelles lignes ou lignes à modifier dans la table stations: {news_rows.shape}")
-    insert_and_update_rows(news_rows, name, primary_key='id')
+  news_rows = search_new_or_updated_rows(df_table=df_table_stations,
+                                        df_entry_data=df_entry_data)
+  
+  # news_rows["date_of_scheduling"] = str_date_ti_timzeone_europe_paris
+  insert_and_update_rows(news_rows, name, primary_key='id')
 
 
 def insert_and_update_rows(df, name, primary_key):
@@ -197,8 +232,8 @@ def load_ruptures_table(dict_df, name):
   
   d_type_ruptures = dtype.copy()
   d_type_ruptures.pop('concatened_id')
-  
   df_table_rupture = get_table_to_dataframe(columns=columns, name=name, dtype=dtype)
+
   df_entry_data = read_data_entry(dict_df=dict_df)
   
   if 'type' not in df_entry_data:
@@ -244,7 +279,7 @@ def stg_get_table_with_where_condition(columns, name, dtype, where_condition, pa
   """.format(columns, name)
 
   total_query = query_select_from + where_condition
-  
+
   try:
       df = pd.read_sql_query(
       sql=total_query,
@@ -258,7 +293,7 @@ def stg_get_table_with_where_condition(columns, name, dtype, where_condition, pa
 
 
 def get_table_to_dataframe(columns, name, dtype):
-  
+    
   query = """
     SELECT
       {}
@@ -272,7 +307,7 @@ def get_table_to_dataframe(columns, name, dtype):
       con=engine,
       dtype=dtype,
     )
-
+      
       return df
     
   except Exception as e:
@@ -300,16 +335,21 @@ def get_entry_data(df, dtype):
 
 
 def search_new_or_updated_rows(df_table, df_entry_data):
-
-
+  
+  if "station_id" in df_table.columns:
+    df_table["station_id"] = pd.to_numeric(df_table["station_id"], downcast='integer')
+    df_entry_data["station_id"] = pd.to_numeric(df_entry_data["station_id"], downcast='integer')
+  
+  
   new_or_updated_lines = pd.merge(df_entry_data, df_table, how="left", indicator=True)
   news_rows = new_or_updated_lines.loc[new_or_updated_lines["_merge"] == "left_only"]
   news_rows.drop(columns="_merge", inplace=True)
-  news_rows["injected_at"] = heure_fr_str
+  if "station_id" in df_table.columns:
+    news_rows["injected_at"] = heure_fr_str
+    
   return news_rows
   
 
-  
 def stg_get_uniques_values_data_entry(dict_df, year_month):
   
   df = pd.DataFrame(dict_df)
@@ -324,13 +364,9 @@ def stg_get_uniques_values_data_entry(dict_df, year_month):
     (df['year_month'] == year_month)
   ]
   
-
-  # list_years_unique_values = list(df['year'].unique())
-  # if len(list_years_unique_values) == 0:
-  #   raise Exception("taille de la liste à 0, il ne peut pas y avoir aucune valeur unique de la colonne year")
-  # return df, list_years_unique_values
   return df
-  
+
+
 def get_date_ti_in_timezone_europe_paris(date_ti):
   
   paris_tz = pytz.timezone("Europe/Paris")
@@ -349,88 +385,10 @@ def stg_query_where_year_is(date_ti):
   WHERE 
     year_month = '{}';
   """.format(year_month)
-  
-  # if len(list_years_unique_values) == 1:
-  #   print("longueur de list years égale à 1")
-  #   where_condition = list_years_unique_values[0]
-  #   print(where_condition)
-  #   query = """
-  #   WHERE
-  #     year = {}
-  #   """.format(where_condition)
-  #   return query
-    
-  # elif len(list_years_unique_values) > 1:
-  #   print(list_years_unique_values)
-  #   min_year = min(list_years_unique_values)
-  #   print(min_year)
-  #   max_year = max(list_years_unique_values)
-  #   print(max_year)
-  #   query = """
-  #   WHERE
-  #     year BETWEEN {} AND {};
-  #   """.format(min_year, max_year)
-  #   return query
-  
+
   return year_month, where_condition
   
     
-
-
-
-
-
-
-
-def if_table_exists(dict_df, name, date_ti, primary_key):
-
-  current_date = datetime.now().date()
-  date_string = current_date.strftime("%Y-%m-%d")
-  
-  if name == 'prix':
-  
-      df_query = pd.read_sql_query(
-      # sql=f"SELECT * FROM dev.{name} WHERE date_maj = '{date_string}'",
-      sql=f"SELECT * FROM dev.{name} WHERE year = 2012",
-      con=engine
-    )
-      print(df_query.shape)
-      return df_query
-  
-  try:
-      df_query = pd.read_sql_query(
-      sql=f"SELECT * FROM dev.{name}",
-      con=engine
-    )
-      return df_query
-  
-  except:
-    df = pd.DataFrame(dict_df)
-    df["injected_at"] = heure_fr
-    if name == 'stations':
-      df["date_of_scheduling"] = date_ti
-      df["date_of_scheduling"] = pd.to_datetime(df["date_of_scheduling"], format="ISO8601")
-      df['id'] = df["id"].astype('int')
-    print("first load")
-          
-    df.to_sql(
-      name=name,
-      con=engine,
-      if_exists="replace",
-      schema="dev",
-      method=psql_insert_copy,
-      index=False
-    )
-    if primary_key:
-      query = """
-        ALTER TABLE dev.{}
-        ADD PRIMARY KEY (id);
-      """.format(name)
-      with psycopg.connect(conn_psycopg) as conn:
-        with conn.cursor() as cur:
-          cur.execute(query)
-
-
 def concatenate_primary_key(df):
   
   df['concatened_id'] = df['id'] + "-" + df['debut'] + "-" + df['station_id']
